3d9761a5261e790d8b380bcfa0f94cb158fb14c5
Abhinav Singh <abhinav.42.singh@gmail.com>
2026-03-03 19:24:49 -0800

    Simplify code for arrow annotations

--- src/components/annotations/Arrow.tsx
+++ src/components/annotations/Arrow.tsx
@@ -9,11 +9,12 @@ type ArrowProps = {
 
 export function Arrow({ position, direction, length = 2, color = 'red' }: ArrowProps) {
   const offsetDistance = length;
+
+  // Flip arrow with origin at offset so it's pointing at model
   const origin = position.clone().addScaledVector(
     direction.normalize(),
     offsetDistance
   );
-
   const flipDir = direction.normalize().multiplyScalar(-1);
 
   return (
